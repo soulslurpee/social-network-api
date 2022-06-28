91f079dbe3f9b1a349d5e234525c438eeb9366d5
soulslurpee <charles.d.thompson87@gmail.com>
2022-06-28 12:45:23 -0400

    added Reactions.js to models

---
+++ models/Reactions.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionsSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+
+    username: {
+      type: String,
+      required: true
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+module.exports = ReactionsSchema;
