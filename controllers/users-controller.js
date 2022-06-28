aa340a2a6f60941548f4f70d097ccd34bed65af6
soulslurpee <charles.d.thompson87@gmail.com>
2022-06-28 13:04:14 -0400

    updated users-controller.js

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,33 +1,66 @@
 const { User, Users } = require('../models');
 
 const usersController = {
+
   getAllUsers(req, res) {
     User.find({})
-      .then(users => res.json(users))
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({_id: -1})
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  getUsersByID({ params }, res) {
+    Users.findOne({ _id: params.id })
+          .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({_id: -1})
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ 
+            message: 'No user found with this ID' 
+          });
+          return;
+        } 
+        res.json(dbUserData);
+      })
       .catch(err => res.json(err));
   },
 
-  getUsersById(req, res) {
-    Users.findOne({ _id: req.params.id })
-      .then(user => res.json(user))
+  createUsers({ body }, res) {
+    Users.create(body)
+      .then(dbUserData => res.json(dbUserData))
       .catch(err => res.json(err));
   },
 
-  createUsers(req, res) {
-    Users.create(req.body)
-      .then(user => res.json(user))
+  updateUsers({ params, body }, res) {
+    Users.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ 
+            message: 'No user found with this ID' 
+          });
+          return;
+        }
+        res.json(dbUserData);
+      })
       .catch(err => res.json(err));
   },
 
-  updateUsers(req, res) {
-    Users.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
-      .then(user => res.json(user))
-      .catch(err => res.json(err));
-  },
-
-  deleteUsers(req, res) {
-    Users.findOneAndDelete({ _id: req.params.id })
-      .then(user => res.json(user))
+  deleteUsers({ params }, res) {
+    Users.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => res.json(dbUserData))
       .catch(err => res.json(err));
   },
 
