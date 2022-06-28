79635181850531b580d836bacbaa1739b748c8b3
soulslurpee <charles.d.thompson87@gmail.com>
2022-06-28 13:16:56 -0400

    added user routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const userRoutes = require('./api/users-routes');
+const thoughtRoutes = require('./api/thoughts-routes');
 
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 router.use((req, res) => {
-  res.status(404).send('404: Page not found');
+    res.status(404).send(`404: Page not found`);
 });
 
 module.exports = router;
